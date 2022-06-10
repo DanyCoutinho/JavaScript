223e159dd7c11c71cfc6a62ed2f8a1e1e8fa2e61
Dany Coutinho <dany.coutinho@inallweb.com>
2022-06-10 06:47:49 +0100

    ex009
    Condição composta

---
+++ Aula-011/ex009.js
@@ -0,0 +1,16 @@
+var país = "Brasil"
+
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
+
+/* OU
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro')
+}
+*/
