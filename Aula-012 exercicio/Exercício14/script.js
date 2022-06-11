8bfb9bc13360aba1b32f2f13b1e28c936729bdba
Dany Coutinho <dany.coutinho@outlook.com>
2022-06-11 07:56:40 +0100

    Update script.js

--- Aula-012 exercicio/Exercício14/script.js
+++ Aula-012 exercicio/Exercício14/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.querySelector(`div#msg`)
     var img = window.document.querySelector(`img#imagem`)
     var data = new Date()
-    var hora = 19 //data.getHours()
+    var hora = 25 //data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 6 && hora < 10) {
@@ -13,7 +13,9 @@ function carregar() {
         img.src = 'img/Tarde.png'
     } else if (hora >= 18 && hora < 21 ) {
         img.src = 'img/Por-Do-Sol.png'
-    } else {
+    } else if (hora <= 21 && hora < 6) {
         img.src = 'img/Noite.png'
+    } else {
+        msg.innerHTML = `Erro, o dia só tem 24h`
     }
 }
