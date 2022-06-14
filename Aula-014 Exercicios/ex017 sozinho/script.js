81178df686d8796ccf291aa97c301a017d1b8065
DanyCoutinho <dany.coutinho@inallweb.com>
2022-06-14 12:21:54 +0100

    Exercicio 17 sozinho

--- Aula-014 Exercicios/ex017 sozinho/script.js
+++ Aula-014 Exercicios/ex017 sozinho/script.js
@@ -6,18 +6,29 @@ function Tabuada() {
         window.alert('Por favor escolha um número para a tabuada')
     } else {
         var NT = Number(NumTab.value)
+
+        Tab.innerHTML = ''
+        for (var contador = 1 ; contador <= 10 ; contador ++) {
+            var item = document.createElement('option')
+            item.text = `${NT} x ${contador} = ${NT*contador}`
+            item.value = `Seltab${contador}`
+            Tab.appendChild(item)
+        }
+        /*
         var contador = 1
 
-        SelTab.innerHTML = ''
+        Tab.innerHTML = ''
         while (contador <= 10) {
             // Criando Variável para criar Option dentro do Select
             var item = document.createElement('option')
             // item.text é o texto que vai dentro do option
             item.text = `${NT} x ${contador} = ${NT*contador}`
+            // Criando um value no javaScript para organizar os dados dos resultados, para saber qual o item selecionado
+            item.value = `Tab${contador}`
             // Criando os items enquanto o Contador for menor de 10
-            SelTab.appendChild(item)
+            Tab.appendChild(item)
             // Somar 1 ao contador até 10
             contador++
-        }
+        }*/
     }
 }
--- Aula-014 Exercicios/ex017/script.js
+++ Aula-014 Exercicios/ex017/script.js
@@ -8,12 +8,17 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c <= 10) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
+        while (contador <= 10) {
+            // Criando Variável para criar Option dentro do Select
+            var item = document.createElement('option')
+            // item.text é o texto que vai dentro do option
+            item.text = `${NT} x ${contador} = ${NT*contador}`
+            // Criando um value no javaScript para organizar os dados dos resultados, para saber qual o item selecionado
+            item.value = `Tab${contador}`
+            // Criando os items enquanto o Contador for menor de 10
+            Tab.appendChild(item)
+            // Somar 1 ao contador até 10
+            contador++
         }
     }
 }
