81a73494c6a915b231e2736ee6ce71195dec541c
Dany Coutinho <dany.coutinho@inallweb.com>
2022-06-16 23:29:50 +0100

    Aula016
    Função 2

---
+++ Aula-016/funcao02.js
@@ -0,0 +1,10 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
+
+/*
+    Se passa só um parâmetro devemos prever dando n1=0, n2=0
+    Se um dos parâmetros faltar considerá-los como 0
+*/
