6c11b09401b5bd367ebb309ad530093e4f08e963
Dany Coutinho <dany.coutinho@inallweb.com>
2022-06-16 22:31:04 +0100

    Aula 015
    Vetores

---
+++ Aula-015/ambiente.js
@@ -0,0 +1,10 @@
+let num = [2,4,1,3,5]
+console.log(`O nosso vetor tem os seguintes elementos ${num}.`)
+num[5] = 6
+console.log(`Adicionando o número 6 na posição 5: ${num}.`)
+num.push(7)
+console.log(`Adicionando o número 7 na última posição: ${num}`)
+num.length
+console.log(`O nosso vetor tem ${num.length} elementos.`)
+num.sort()
+console.log(`O nosso vetor organizado ${num}`)
